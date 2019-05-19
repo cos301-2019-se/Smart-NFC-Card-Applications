7fac15098f4f9ab2df45abfbc295723ca0eddf2b
Tjaart <35308889+Tjaart-Booyens@users.noreply.github.com>
2019-05-19 22:25:29 +0200

    Set up the jasmine framework that will be used to run Unit Tests

---
+++ BackEnd/EndPoints/appLogic.spec.js
@@ -0,0 +1,25 @@
+/**
+ *	File Name:      appLogic.spec.js
+ *	Project:        Smart-NFC-Application
+ *	Organization:	VastExpanse
+ *	Copyright:	    © Copyright 2019 University of Pretoria
+ *	Classes:
+ *	Related documents:
+ *
+ *	Update History:
+ *	Date		Author		Version		Changes
+ *	-----------------------------------------------------------------------------------------
+ *	2019/05/19	Tjaart		1.0		    Original
+ *
+ *	Functional Description:	This file will be used to run tests on the functions in
+ *                          appLogic.js
+ *	Error Messages:
+ *	Assumptions: appLogic.js exists
+ *	Constraints:
+ */
+
+describe("Testing JS with Jasmine", function(){
+    it("is as easy as it gets!", function(){
+        expect(true).toBe(true);
+    })
+});

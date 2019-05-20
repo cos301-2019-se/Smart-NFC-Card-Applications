02ab994ed6c057633de4f8e5ea0a18a5968be6c6
Tjaart <35308889+Tjaart-Booyens@users.noreply.github.com>
2019-05-20 22:47:11 +0200

    Added checks to see if valid parameters are sent with calls

--- BackEnd/EndPoints/appLogic.js
+++ BackEnd/EndPoints/appLogic.js
@@ -14,24 +14,20 @@
  *	Functional Description:	This class handles the functionality that will be requested by the
  *                          application to the backend system. It handles functionality like
  *                          when a user requests a business card from the server.
- *	Error Messages:
- *	Assumptions: This file assumes that a there exists a 'sharedLogic' and a 'crudController'
- *               class.
- *	Constraints:
+ *	Error Messages: None
+ *	Assumptions:    None
+ *	Constraints:    None
  */
 
-let SharedLogic = require("./../SharedLogic/sharedLogic.js");
-let demoMode = true;
+const SharedLogic = require("./../SharedLogic/sharedLogic.js");
+const demoMode = true;
 
 /**
  * 	Purpose:    This class handles the functionality that will be requested by the
  *              application to the backend system.
- *
  *	Usage:		This class can be used to retrieve details from the backend system
  *              in order to generate a business card.
- *
  *	@author:	Tjaart Booyens
- *
  *	@version:	1.0
  */
 class AppLogic{
@@ -39,8 +35,8 @@ class AppLogic{
      *  Constructor for the class that sets up certain properties as well as instantiate
      *  a new sharedLogic object.
      *
-     *  @param req string Request sent from the application to the backend system
-     *  @param res string Response sent back to the application
+     *  @param req JSON Request sent from the application to the backend system
+     *  @param res JSON Response sent back to the application
      */
     constructor(req, res){
         this.req = req;
@@ -69,7 +65,6 @@ class AppLogic{
             case "getBusinessCard":
                 this.getBusinessCard();
                 break;
-
             default:
                 this.sharedLogic.endServe(false, "Invalid Endpoint", null);
         }
@@ -79,38 +74,99 @@ class AppLogic{
      *  Function that returns the information needed to generate a business card for
      *  the user who requested it.
      *
-     *  @param employeeId int Employee ID that is sent through with the application
-     *                        request.
+     *  @param employeeId int ID of an employee
+     *
+     *  @return JSON {
+     *                  employeeName: string Name of the employee
+     *                  employeeSurname: string Surname of the employee
+     *                  cellphone: string Cellphone number of the employee
+     *                  email: string Email of the employee
+     *                  companyName: string Name of the company
+     *                  website: string Link to the company's website
+     *               }
      */
     getBusinessCard(){
-        let data = {};
+        let success;
         let message;
+        let data = {};
 
-        if(demoMode){
-            // return mock data
-            data.employeeName = "Tjaart";
-            data.employeeSurname = "Booyens";
-            data.companyName = "Vast Expanse";
-            data.cellphone = "0791807734";
-            data.email = "u17021775@tuks.co.za";
-            data.website = "https://github.com/cos301-2019-se/Smart-NFC-Card-Applications";
-            message = "Mock business card information loaded successfully";
+        // check to see if parameters are present
+        let presentParams = false;
+        let presentReturn = "";
+
+        if(!this.body.employeeId){
+            presentParams = true;
+            presentReturn += "employeeId ";
+        }
+
+        // check if the parameters are valid if present
+        if(!presentParams){
+            let invalidParams = false;
+            let invalidReturn = "";
+
+            if(!this.sharedLogic.validateNonEmpty(this.body.employeeId) || !this.sharedLogic.validateNumeric(this.body.employeeId)){
+                invalidParams = true;
+                invalidReturn += "employeeId ";
+            }
+
+            if(!invalidParams){
+                if(demoMode){
+                    // return mock data
+                    data.employeeName = "Tjaart";
+                    data.employeeSurname = "Booyens";
+                    data.cellphone = "0791807734";
+                    data.email = "u17021775@tuks.co.za";
+                    data.companyName = "Vast Expanse";
+                    data.website = "https://github.com/cos301-2019-se/Smart-NFC-Card-Applications";
+
+                    message = "Business card information loaded successfully - Mock";
+                    success = true;
+                }
+                else{
+                    // return data from crudController
+                    let employeeData = this.sharedLogic.crudController.getEmployee(this.body.employeeId);
+
+                    if(employeeData.success){
+                        let companyData = this.sharedLogic.crudController.getCompany(employeeData.companyId);
+
+                        if(companyData.success){
+                            success = true;
+                            message = "Business card information loaded successfully";
+                            data.employeeName = employeeData.employeeName;
+                            data.employeeSurname = employeeData.employeeSurname;
+                            data.cellphone = employeeData.cellphone;
+                            data.email = employeeData.email;
+                            data.companyName = companyData.companyName;
+                            data.website = companyData.website;
+                        }
+                        else{
+                            success = companyData.success;
+                            message = companyData.message;
+                            data = companyData.data;
+                        }
+                    }
+                    else{
+                        success = employeeData.success;
+                        message = employeeData.message;
+                        data = employeeData.data;
+                    }
+                }
+            }
+            else{
+                success = false;
+                message = "Invalid Parameters: " + invalidReturn;
+                message = message.slice(0, message.length-1);
+                data = null;
+            }
         }
         else{
-            // get data by calling the crud controller
-            let employeeData = this.sharedLogic.crudController.getEmployee(this.body.employeeId);
-            let companyData = this.sharedLogic.crudController.getCompany(employeeData.companyId);
-
-            data.employeeName = employeeData.employeeName;
-            data.employeeSurname = employeeData.employeeSurname;
-            data.cellphone = employeeData.cellphone;
-            data.email = employeeData.email;
-            data.companyName = companyData.companyName;
-            data.website = companyData.website;
-            message = "Business card information loaded successfully";
+            success = false;
+            message = "Missing Parameters: " + presentReturn;
+            message = message.slice(0, message.length-1);
+            data = null;
         }
 
-        this.sharedLogic.endServe(true, message, data);
+        this.sharedLogic.endServe(success, message, data);
     }
 }
 

eed3d63b30b679ed7b3a42e2165176db6d19602c
jared2710 <u17051429@tuks.co.za>
2019-05-22 01:22:23 +0200

    Added unit tests for general checks in sharedLogic as well as a valid and invalid login for both app and login, also in sharedLogic. Also checks for invalid endpoints from server.js. Removed unit tests for testLogic.js as causing errors which will not be fixed as we will eventually delete testLogic.js

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -10,7 +10,8 @@
  *	Date		Author		Version		Changes
  *	-----------------------------------------------------------------------------------------
  *	2019/05/19	Tjaart		1.0		    Original
- *  2019/05/20  Duncan     1.1         Fixed posting to endpoints
+ *  2019/05/20  Duncan     	1.1         Fixed posting to endpoints
+ *	2019/05/22	Jared		1.2			Added unit tests for sharedLogic and server
  *
  *	Functional Description:	This file is used to run Unit tests on the server
  *	Error Messages:
@@ -67,6 +68,7 @@ describe('Server.js Unit Testing', function(){
         })
     });
 
+/*
     //http://localhost:3000/test - {}
     describe("POST " + endpoint + "/test", function () {
         let data = {};
@@ -143,6 +145,8 @@ describe('Server.js Unit Testing', function(){
             expect(data.body['data']).toEqual({});
         })
     });
+	
+	*/
 
     describe("POST " + endpoint + "/admin/addCompany", function () {
         let data = new Object();
@@ -323,4 +327,442 @@ describe('Server.js Unit Testing', function(){
 
         });
     });
+	
+
+
+
+	//jared
+    describe("POST " + endpoint + "/app/getBusinessCard", function () {
+        let data = new Object();
+        beforeAll(function(done){
+            var jsonDataObj = {}; // no data sent
+            Request.post({
+                url: endpoint + "/app/getBusinessCard",
+                body: jsonDataObj,
+                json: true
+            }, function(error, response, body){
+                data.status = response.statusCode;
+                data.contentType = response.headers['content-type'];
+                data.body = response.body;
+                done();
+            });
+        });
+
+        it('should return with statusCode 200', function(){
+            expect(data.status).toEqual(200);
+        });
+
+        it('should set content type = application/json', function(){
+            expect(data.contentType).toEqual('application/json');
+        });
+
+        it('should return a json object \n\t{\n\t' +
+            '    "success": false,\n\t' +
+            '    "message": "No API Key or not all login details provided",\n\t' +
+            '    "data": {}\n\t' +
+            '}' , function () {
+            expect(data.body).toEqual(
+			{
+				"success": false,
+				"message": "No API Key or not all login details provided",
+				"data": {}
+			})
+
+        });
+    });
+
+
+    describe("POST " + endpoint + "/app/getBusinessCard", function () {
+        let data = new Object();
+        beforeAll(function(done){
+            var jsonDataObj = {
+				username: "jaredoreilly@gmail.com"
+			}; // no data sent
+            Request.post({
+                url: endpoint + "/app/getBusinessCard",
+                body: jsonDataObj,
+                json: true
+            }, function(error, response, body){
+                data.status = response.statusCode;
+                data.contentType = response.headers['content-type'];
+                data.body = response.body;
+                done();
+            });
+        });
+
+        it('should return with statusCode 200', function(){
+            expect(data.status).toEqual(200);
+        });
+
+        it('should set content type = application/json', function(){
+            expect(data.contentType).toEqual('application/json');
+        });
+
+        it('should return a json object \n\t{\n\t' +
+            '    "success": false,\n\t' +
+            '    "message": "No API Key or not all login details provided",\n\t' +
+            '    "data": {}\n\t' +
+            '}' , function () {
+            expect(data.body).toEqual(
+			{
+				"success": false,
+				"message": "No API Key or not all login details provided",
+				"data": {}
+			})
+
+        });
+    });
+
+
+    describe("POST " + endpoint + "/app/getBusinessCard", function () {
+        let data = new Object();
+        beforeAll(function(done){
+            var jsonDataObj = {
+				password: "CoolPassword1"
+			}; // no data sent
+            Request.post({
+                url: endpoint + "/app/getBusinessCard",
+                body: jsonDataObj,
+                json: true
+            }, function(error, response, body){
+                data.status = response.statusCode;
+                data.contentType = response.headers['content-type'];
+                data.body = response.body;
+                done();
+            });
+        });
+
+        it('should return with statusCode 200', function(){
+            expect(data.status).toEqual(200);
+        });
+
+        it('should set content type = application/json', function(){
+            expect(data.contentType).toEqual('application/json');
+        });
+
+        it('should return a json object \n\t{\n\t' +
+            '    "success": false,\n\t' +
+            '    "message": "No API Key or not all login details provided",\n\t' +
+            '    "data": {}\n\t' +
+            '}' , function () {
+            expect(data.body).toEqual(
+			{
+				"success": false,
+				"message": "No API Key or not all login details provided",
+				"data": {}
+			})
+
+        });
+    });
+
+
+    describe("POST " + endpoint + "/app", function () {
+        let data = new Object();
+        beforeAll(function(done){
+            var jsonDataObj = {}; // no data sent
+            Request.post({
+                url: endpoint + "/app",
+                body: jsonDataObj,
+                json: true
+            }, function(error, response, body){
+                data.status = response.statusCode;
+                data.contentType = response.headers['content-type'];
+                data.body = response.body;
+                done();
+            });
+        });
+
+        it('should return with statusCode 200', function(){
+            expect(data.status).toEqual(200);
+        });
+
+        it('should set content type = application/json', function(){
+            expect(data.contentType).toEqual('application/json');
+        });
+
+        it('should return a json object \n\t{\n\t' +
+            '    "success": false,\n\t' +
+            '    "message": "Invalid Endpoint",\n\t' +
+            '    "data": {}\n\t' +
+            '}' , function () {
+            expect(data.body).toEqual(
+			{
+				"success": false,
+				"message": "Invalid Endpoint",
+				"data": {}
+			})
+
+        });
+    });
+
+
+    describe("POST " + endpoint + "/blahblah", function () {
+        let data = new Object();
+        beforeAll(function(done){
+            var jsonDataObj = {}; // no data sent
+            Request.post({
+                url: endpoint + "/blahblah",
+                body: jsonDataObj,
+                json: true
+            }, function(error, response, body){
+                data.status = response.statusCode;
+                data.contentType = response.headers['content-type'];
+                data.body = response.body;
+                done();
+            });
+        });
+
+        it('should return with statusCode 200', function(){
+            expect(data.status).toEqual(200);
+        });
+
+        it('should set content type = application/json', function(){
+            expect(data.contentType).toEqual('application/json');
+        });
+
+        it('should return a json object \n\t{\n\t' +
+            '    "success": false,\n\t' +
+            '    "message": "Invalid Endpoint",\n\t' +
+            '    "data": {}\n\t' +
+            '}' , function () {
+            expect(data.body).toEqual(
+			{
+				"success": false,
+				"message": "Invalid Endpoint",
+				"data": {}
+			})
+
+        });
+    });
+
+
+    describe("POST " + endpoint + "/wrong/getBusinessCard", function () {
+        let data = new Object();
+        beforeAll(function(done){
+            var jsonDataObj = {}; // no data sent
+            Request.post({
+                url: endpoint + "/wrong/getBusinessCard",
+                body: jsonDataObj,
+                json: true
+            }, function(error, response, body){
+                data.status = response.statusCode;
+                data.contentType = response.headers['content-type'];
+                data.body = response.body;
+                done();
+            });
+        });
+
+        it('should return with statusCode 200', function(){
+            expect(data.status).toEqual(200);
+        });
+
+        it('should set content type = application/json', function(){
+            expect(data.contentType).toEqual('application/json');
+        });
+
+        it('should return a json object \n\t{\n\t' +
+            '    "success": false,\n\t' +
+            '    "message": "Invalid Endpoint",\n\t' +
+            '    "data": {}\n\t' +
+            '}' , function () {
+            expect(data.body).toEqual(
+			{
+				"success": false,
+				"message": "Invalid Endpoint",
+				"data": {}
+			})
+
+        });
+    });
+
+
+
+    describe("POST " + endpoint + "/app/login", function () {
+        let data = new Object();
+        beforeAll(function(done){
+            var jsonDataObj = {
+				username : "jaredoreilly@gmail.com",
+				password : "CoolPassword1",
+				demoMode : true
+			}; // no data sent
+            Request.post({
+                url: endpoint + "/app/login",
+                body: jsonDataObj,
+                json: true
+            }, function(error, response, body){
+                data.status = response.statusCode;
+                data.contentType = response.headers['content-type'];
+                data.body = response.body;
+                done();
+            });
+        });
+
+        it('should return with statusCode 200', function(){
+            expect(data.status).toEqual(200);
+        });
+
+        it('should set content type = application/json', function(){
+            expect(data.contentType).toEqual('application/json');
+        });
+
+        it('should return a json object \n\t{\n\t' +
+            '    "success": true,\n\t' +
+            '    "message": "Login successful.",\n\t' +
+            '    "data": {"apiKey": "1234",\n\t' +
+			'		"id": 45}\n\t' +
+            '}' , function () {
+            expect(data.body).toEqual(
+			{
+				"success": true,
+				"message": "Login successful.",
+				"data": {
+					"apiKey": "1234",
+					"id": 45
+				}
+			})
+
+        });
+    });
+
+    describe("POST " + endpoint + "/app/login", function () {
+        let data = new Object();
+        beforeAll(function(done){
+            var jsonDataObj = {
+				username : "jaredoreilly@gmail.com",
+				password : "WrongPassword",
+				demoMode : true
+			}; // no data sent
+            Request.post({
+                url: endpoint + "/app/login",
+                body: jsonDataObj,
+                json: true
+            }, function(error, response, body){
+                data.status = response.statusCode;
+                data.contentType = response.headers['content-type'];
+                data.body = response.body;
+                done();
+            });
+        });
+
+        it('should return with statusCode 200', function(){
+            expect(data.status).toEqual(200);
+        });
+
+        it('should set content type = application/json', function(){
+            expect(data.contentType).toEqual('application/json');
+        });
+
+        it('should return a json object \n\t{\n\t' +
+            '    "success": true,\n\t' +
+            '    "message": "Incorrect username and/or password.",\n\t' +
+            '    "data": {}\n\t' +
+            '}' , function () {
+            expect(data.body).toEqual(
+			{
+				"success": false,
+				"message": "Incorrect username and/or password.",
+				"data": {}
+			})
+
+        });
+    });
+
+
+
+    describe("POST " + endpoint + "/admin/login", function () {
+        let data = new Object();
+        beforeAll(function(done){
+            var jsonDataObj = {
+				username : "company@discovery.com",
+				password : "Discovery15Cool",
+				demoMode : true
+			}; // no data sent
+            Request.post({
+                url: endpoint + "/admin/login",
+                body: jsonDataObj,
+                json: true
+            }, function(error, response, body){
+                data.status = response.statusCode;
+                data.contentType = response.headers['content-type'];
+                data.body = response.body;
+                done();
+            });
+        });
+
+        it('should return with statusCode 200', function(){
+            expect(data.status).toEqual(200);
+        });
+
+        it('should set content type = application/json', function(){
+            expect(data.contentType).toEqual('application/json');
+        });
+
+        it('should return a json object \n\t{\n\t' +
+            '    "success": true,\n\t' +
+            '    "message": "Login successful.",\n\t' +
+            '    "data": {"apiKey": "5678",\n\t' +
+			'		"id": 3}\n\t' +
+            '}' , function () {
+            expect(data.body).toEqual(
+			{
+				"success": true,
+				"message": "Login successful.",
+				"data": {
+					"apiKey": "5678",
+					"id": 3
+				}
+			})
+
+        });
+    });
+
+    describe("POST " + endpoint + "/admin/login", function () {
+        let data = new Object();
+        beforeAll(function(done){
+            var jsonDataObj = {
+				username : "company@discovery.com",
+				password : "WrongPassword",
+				demoMode : true
+			}; // no data sent
+            Request.post({
+                url: endpoint + "/admin/login",
+                body: jsonDataObj,
+                json: true
+            }, function(error, response, body){
+                data.status = response.statusCode;
+                data.contentType = response.headers['content-type'];
+                data.body = response.body;
+                done();
+            });
+        });
+
+        it('should return with statusCode 200', function(){
+            expect(data.status).toEqual(200);
+        });
+
+        it('should set content type = application/json', function(){
+            expect(data.contentType).toEqual('application/json');
+        });
+
+        it('should return a json object \n\t{\n\t' +
+            '    "success": true,\n\t' +
+            '    "message": "Incorrect username and/or password.",\n\t' +
+            '    "data": {}\n\t' +
+            '}' , function () {
+            expect(data.body).toEqual(
+			{
+				"success": false,
+				"message": "Incorrect username and/or password.",
+				"data": {}
+			})
+
+        });
+    });
+
+
+
+
+    
+
+	
 });

9d5adbb5ab1d80ccdd5a8ce1d3507e5a19046f9e
jared2710 <u17051429@tuks.co.za>
2019-05-21 23:30:53 +0200

    Added a file description header to server.js as well as function comments to describe the routing function

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,23 +1,54 @@
-// test application to test heroku
+/**
+ *	File Name:      server.js
+ *	Project:        Smart-NFC-Application
+ *	Organization:	VastExpanse
+ *	Copyright:	    © Copyright 2019 University of Pretoria
+ *	Classes:	    
+ *	Related documents:
+ *
+ *	Update History:
+ *	Date		Author		Version		Changes
+ *	-----------------------------------------------------------------------------------------
+ *	2019/05/17	Tjaart		1.0		    Original
+ *	2019/05/19	Jared		1.1			Added routing capabilities to the app and admin subsystems
+										and returns errors if invalid subsystem specified
+ *	2019/05/21	Duncan		1.2			Added callback functions to enable server to be run with tests
+ *
+ *	Functional Description:	This class is used by the (other)Logic.js files i.e. the (Other)Logic
+							classes, and performs some common request data parsing, extraction, validation,
+							authentication and response functions for these (Other)Logic classes.
+							They include this class and make a new instance of it, allowing them to
+							use the functionality defined here (hence, SHARED).
+ *	Error Messages:
+ *	Assumptions: This file assumes that a there exists 'AdminLogic' and 'AppLogic' classes.
+ *	Constraints:
+ */
 
 const http = require('http');
 const port=process.env.PORT || 3000;
 
-// to enable the server to be run with tests
+/**
+ *  This run function takes in a callback function pointer, enabling the server to be run with tests.
+ *	This function extracts the first part of the url (i.e. the desired subsystem) and plugs it into a
+ *	switch statement, which creates an instance of the appropriate class and calls the handle() method
+ *	of the class, effectively routing the request to that class and allowing it to handle the response.
+ *	If an invalid subsystem is specified, then a failure response is returned to the caller.
+ *
+ *  @param callback function The function to be called after the server has started that executes tests
+ *	@return the created http server
+*/
+	
 function run(callback) {
 	const server = http.createServer((req, res) => {
 
-		//routes the url to the correct subsystem
-		//looks at the first part (e.g. /app/endPoint1 would be app) and routes it based on that
-		//also returns an error page with 404 if not a valid first part and second part not included
 		var urlToSwitchOn = req.url.substring(1, req.url.substring(1).indexOf("/")+1);
 		switch(urlToSwitchOn)
 		{
-			case "test":
+			/*case "test":
 				var TestLogic = require('./EndPoints/testLogic.js');
 				var testLogic = new TestLogic(req, res);
 				testLogic.handle();
-				break;
+				break;*/
 
 			case "app":
 				var AppLogic = require('./EndPoints/appLogic.js');

1be28d5fc800ade5f6449186ef926d273ba0b1b6
DuncanV <duncanvodden999@gmail.com>
2019-07-13 18:07:00 +0200

    added google maps api

--- AdminInterface/Js/buildings.js
+++ AdminInterface/Js/buildings.js
@@ -201,6 +201,7 @@ function addBuilding()
 
 function editBuilding(buildingId)
 {
+
 	console.log(buildingId);
 	
 	var api = localStorage.getItem("apiKey");
@@ -217,7 +218,8 @@ function editBuilding(buildingId)
 		{
 			data = data.data;
 			console.log(data);
-			
+
+			$("#editAddress").val("");
 			$("#buildingNameE").val(data.branchName);
 			$("#latE").val(data.latitude);
 			$("#longiE").val(data.longitude);
@@ -229,6 +231,7 @@ function editBuilding(buildingId)
 			localStorage.setItem("currentWifiParamsEdit", data.wifiParamsId);
 			
 			$("#editBuildingModal").modal('show');
+			initEditAutocomplete();
 			
 			//window.location.replace("buildings.html");
         } 
@@ -428,8 +431,162 @@ function addRoom()
 	
 
 }
+function initAddAutocomplete() {
+	$("#lat").val("");
+	$("#longi").val("");
+	$("#addAddress").val("");
+	//creates inital zoomed out map
+       var map = new google.maps.Map(document.getElementById('addMap'), {
+           center: {lat: -0.0, lng: 0.0},
+           zoom: 2,
+           mapTypeId: 'roadmap'
+       });
+
+	// Create the search box and link it to the search bar element.
+	var input = document.getElementById('addAddress');
+	var searchBox = new google.maps.places.SearchBox(input);
+
+	// Bias the SearchBox results towards current map's viewport.
+       map.addListener('bounds_changed', function() {
+           searchBox.setBounds(map.getBounds());
+       });
+
+       var markers = [];
+	// Listen for the event fired when the user selects a prediction and retrieve
+	// more details for that place.
+	searchBox.addListener('places_changed', function() {
+		var places = searchBox.getPlaces();
+
+		if (places.length == 0) {
+			return;
+		}
+
+		// Clear out the old markers.
+           markers.forEach(function(marker) {
+               marker.setMap(null);
+           });
+           markers = [];
+
+           // For each place, get the icon, name and location.
+           var bounds = new google.maps.LatLngBounds();
+           places.forEach(function(place) {
+               if (!place.geometry) {
+                   console.log("Returned place contains no location");
+                   return;
+               }
+
+               // Create a marker for the location
+               markers.push(new google.maps.Marker({
+                   map: map,
+                   title: place.name,
+                   animation: google.maps.Animation.DROP,
+                   position: place.geometry.location
+               }));
+
+               // get lat and long
+               $("#lat").val(place.geometry.location.lat());
+			   $("#longi").val(place.geometry.location.lng());
+
+               if (place.geometry.viewport) {
+                   // Only geocodes have viewport.
+                   bounds.union(place.geometry.viewport);
+               } else {
+                   bounds.extend(place.geometry.location);
+               }
+           });
+           map.fitBounds(bounds);
+	});
+}
+
+function initEditAutocomplete() {
+
+	var markers = [];
+	var geocoder = new google.maps.Geocoder;
+	let lat = $("#latE").val();
+	let long = $("#longiE").val();
+	let latlong = {lat:parseFloat(lat), lng:parseFloat(long)};
+	//creates inital zoomed out map
+	var map = new google.maps.Map(document.getElementById('editMap'), {
+		center: latlong,
+		zoom: 15,
+		mapTypeId: 'roadmap'
+	});
+
+	geocoder.geocode({'location': latlong}, function(results, status) {
+		if (status === 'OK') {
+			if(results[0]){
+				$("#editAddress").val(results[0].formatted_address);
+				markers.push(new google.maps.Marker({
+					position: latlong,
+					map: map,
+					title: results[0].formatted_address,
+					animation: google.maps.Animation.DROP,
+				}));
+			}else{
+				console.log("Could'nt find address")
+			}
+		}else{
+			console.log("Could'nt find address")
+		}
+	});
 
 
+	// Create the search box and link it to the search bar element.
+	var input = document.getElementById('editAddress');
+	var searchBox = new google.maps.places.SearchBox(input);
+
+	// Bias the SearchBox results towards current map's viewport.
+	map.addListener('bounds_changed', function() {
+		searchBox.setBounds(map.getBounds());
+	});
+
+
+	// Listen for the event fired when the user selects a prediction and retrieve
+	// more details for that place.
+	searchBox.addListener('places_changed', function() {
+		var places = searchBox.getPlaces();
+
+		if (places.length == 0) {
+			return;
+		}
+
+		// Clear out the old markers.
+		markers.forEach(function(marker) {
+			marker.setMap(null);
+		});
+		markers = [];
+
+		// For each place, get the icon, name and location.
+		var bounds = new google.maps.LatLngBounds();
+		places.forEach(function(place) {
+			if (!place.geometry) {
+				console.log("Returned place contains no location");
+				return;
+			}
+
+
+			// Create a marker for the location
+			markers.push(new google.maps.Marker({
+				map: map,
+				title: place.name,
+				animation: google.maps.Animation.DROP,
+				position: place.geometry.location
+			}));
+
+			// get lat and long
+			$("#latE").val(place.geometry.location.lat());
+			$("#longiE").val(place.geometry.location.lng());
+
+			if (place.geometry.viewport) {
+				// Only geocodes have viewport.
+				bounds.union(place.geometry.viewport);
+			} else {
+				bounds.extend(place.geometry.location);
+			}
+		});
+		map.fitBounds(bounds);
+	});
+}
 
 function logout(){
 	localStorage.clear();

fda0be030b538bfff6f4ec76274adcfcef15c347
jared2710 <u17051429@tuks.co.za>
2019-07-25 09:51:27 +0200

    Trying to fix with just database url

--- BackEnd/CrudController/crudController.js
+++ BackEnd/CrudController/crudController.js
@@ -73,14 +73,22 @@ class CrudController {
 			ssl: true
 		});*/
 
-		this.client.connect()
+		this.client.connect();
+		
+		/*
 		.catch(err => 
 		{
 			this.client = new Client({
 				connectionString: "postgres://postgres:nbuser@localhost:5432/link"
 			});
 			return this.client.connect()
+			
+		})
+		.catch(err =>
+		{
+			
 		});
+		*/
 		
 		
 
